5a48feca8e93e1ad9b29be0ca0a93b03dfa70839
ElinAxelsson <elin_ax@outlook.com>
2025-10-21 10:49:08 +0200

    Delete logout.php

--- logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-session_unset();  // Rensa alla sessioner
-session_destroy();  // Zappa sessionen
-header('Location: login.php');  // Omdirigera till login-sidan
-exit();
-?>
